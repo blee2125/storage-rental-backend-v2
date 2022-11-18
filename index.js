b81f229bdf21e2ab0985ab098208e4a4a94c99ec
Brandon Lee <blee2125@gmail.com>
2022-11-18 16:56:10 -0700

    connect database and added routes

--- index.js
+++ index.js
@@ -9,24 +9,21 @@ const mongoDatabase = process.env.DATABASE_URL;
 mongoose.connect(mongoDatabase);
 const database = mongoose.connection;
 
-database.on('error', (error) => {
-    console.log(error)
-})
-database.once('connected', () => {
-    console.log('Database Connected');
-})
+database.on('error', (error) => {console.log(error)})
+database.once('connected', () => {console.log('Database Connected')})
 
 // Express
 const app = express();
-
 app.use(express.json());
 app.use(cors())
 
 // Port
 var port = process.env.PORT || 8000;
-
-app.listen(port, () => {
-    console.log(`Server Started at ${port}`)
-})
+app.listen(port, () => {console.log(`Server Started at ${port}`)})
 
 // Routes
+const StorageUnit = require('./routes/StorageUnit');
+app.use('/api/storage-unit', StorageUnit)
+
+const Customer = require('./routes/Customer');
+app.use('/api/customer', Customer)
